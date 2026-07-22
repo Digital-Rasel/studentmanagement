ed4a097222f909351b028fcd21320ad482f0f54a
Digital-Rasel <raselahmmedbaf@gmail.com>
2026-07-22 12:25:22 +0600

    Another chapter completed

--- login.php
+++ login.php
@@ -6,12 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+<style>
+    h6{
+        color: red;
+    }
+</style>
 </head>
 <body background="login.jpg" class="body_deg">
     <center>
         <div class="form_deg">
             <center class="title_deg">
                 Login Form
+                <h6>
+                    <?php
+                    error_reporting(0);
+                    session_start();
+                    session_destroy();
+                    echo $_SESSION["loginMessage"];
+                    ?>
+                </h6>
             </center>
             <form action="login_check.php" method="POST" class="login_form">
                 <div>
