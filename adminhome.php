6f023decdff0d92fc8d20eb597d09a064963f89a
Digital-Rasel <raselahmmedbaf@gmail.com>
2026-07-23 14:27:17 +0600

    Admission form to database connection

--- adminhome.php
+++ adminhome.php
@@ -12,12 +12,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="admin.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
 </head>
 <body>
-    <h1>Admin Home</h1>
-    <a href="logout.php">Logout</a>
+    <header class="header">
+        <a href="">Admin Dashboard</a>
+        <div class="logout">
+        <a href="logout.php" class="btn btn-primary">Logout</a>
+    </div>
+    </header>
+    <aside>
+        <ul>
+            <li>
+                <a href="">Admission</a>
+            </li>
+             <li>
+                <a href="">Add Student</a>
+            </li>
+             <li>
+                <a href="">View Student</a>
+            </li>
+             <li>
+                <a href="">Add Teacher</a>
+            </li>
+             <li>
+                <a href="">View Teacher</a>
+            </li>
+            <li>
+                <a href="">Add Course</a>
+            </li>
+            <li>
+                <a href="">View Course</a>
+            </li>
+        </ul>
+    </aside>
+    
+
 </body>
 </html>
--- studenthome.php
+++ studenthome.php
@@ -12,12 +12,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="admin.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Dashboard</title>
 </head>
 <body>
-    <h1>Student Home</h1>
-    <a href="logout.php">Logout</a>
+    <header class="header">
+        <a href="">Student Dashboard</a>
+        <div class="logout">
+        <a href="logout.php" class="btn btn-primary">Logout</a>
+    </div>
+    </header>
+    <aside>
+        <ul>
+            <li>
+                <a href="">My Course</a>
+            </li>
+             <li>
+                <a href="">My Result</a>
+            </li>
+        </ul>
+    </aside>
+    
+
 </body>
 </html>
